07cf909b34f6b9c3befa62dc7fe84e39079cf007
Liton124 <92647972+Liton124@users.noreply.github.com>
2021-11-20 00:09:59 +0530

    Update Play.ts

--- src/commands/Media/Play.ts
+++ src/commands/Media/Play.ts
@@ -12,7 +12,10 @@ export default class Command extends BaseCommand {
             command: 'play',
             description: '🎵 play a song with just search term!',
             category: 'media',
-            aliases: ['music'],
+            aliases: ['music', 'song']
+
+
+
             usage: `${client.config.prefix}play [term]`,
             baseXp: 30
         })
@@ -25,14 +28,14 @@ export default class Command extends BaseCommand {
         if (!videos || videos.length <= 0) return void M.reply(`⚓ No Matching videos found for the term : *${term}*`)
         const audio = new YT(videos[0].url, 'audio')
         if (!audio.url) return
-        M.reply('👾 Sending...')
+        M.reply('👾 Sending your song, wait for a while.')
         this.client
             .sendMessage(M.from, await audio.getBuffer(), MessageType.audio, {
                 quoted: M.WAMessage,
                 contextInfo: {
                     externalAdReply: {
-                        title: videos[0].title.substr(0, 30),
-                        body: `author : ${videos[0].author.name.substr(0, 20)}\nSent Via : Kaoi`,
+                        title: videos[0].title.substr(0, 50),
+                        body: `author : ${videos[0].author.name.substr(0, 35)}\nSent Via : Kaoi`,
                         mediaType: 2,
                         thumbnailUrl: `https://i.ytimg.com/vi/${audio.id}/hqdefault.jpg`,
                         mediaUrl: audio.url
