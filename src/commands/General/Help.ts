0f8205432a385c86386d4d53edfb9ef83c864bff
Liton124 <92647972+Liton124@users.noreply.github.com>
2021-11-20 00:56:33 +0530

    Update Help.ts

--- src/commands/General/Help.ts
+++ src/commands/General/Help.ts
@@ -29,7 +29,7 @@ export default class Command extends BaseCommand {
                     categories[info.config.category].push(info)
                 }
             }
-            let text = `👋🏻 (💙ω💙) Konnichiwa! *${M.sender.username}*, I'm Kaoi.\n\nMy prefix is - "!"\n\nThe usable commands are listed below.\n\n(Use these commands only, Don't spam unknown commands)`
+            let text = `👋🏻 (💙ω💙) Konnichiwa! *${M.sender.username}*, I'm Kaoi.\n\nMy prefix is - "!"\n\nThe usable commands are listed below.\n\n Use these commands only!!!`
             const keys = Object.keys(categories).sort((a, b) => a.localeCompare(b))
             for (const key of keys)
                 text += `${this.emojis[keys.indexOf(key)]} *${this.client.util.capitalize(key)}*\n❐ \`\`\`${categories[
