c135f3eeba31d26077a4c4af6bac92e12fa8752a
Liton124 <92647972+Liton124@users.noreply.github.com>
2021-11-19 18:51:37 +0530

    Update Open.ts

--- src/commands/Moderation/Open.ts
+++ src/commands/Moderation/Open.ts
@@ -18,8 +18,8 @@ export default class Command extends BaseCommand {
 
     run = async (M: ISimplifiedMessage): Promise<void> => {
         if (!M.groupMetadata?.admins?.includes(this.client.user.jid))
-            return void M.reply("I can't open the group without being an admin")
-        if (M.groupMetadata.announce === 'false') return void M.reply('Group is already open')
+            return void M.reply("Make me admin first for this command, Baka")
+        if (M.groupMetadata.announce === 'false') return void M.reply('Group is already opened, Baka')
 
         this.client.groupSettingChange(M.groupMetadata.id, GroupSettingChange.messageSend, false)
     }
