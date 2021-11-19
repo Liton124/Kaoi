500a4be3328759b523299cca085e89343a0675cd
Liton124 <92647972+Liton124@users.noreply.github.com>
2021-11-19 19:19:19 +0530

    Update EventHandler.ts

--- src/Handlers/EventHandler.ts
+++ src/Handlers/EventHandler.ts
@@ -17,13 +17,11 @@ export default class EventHandler {
         if (!data.events) return void null
         const add = event.action === 'add'
         const text = add
-            ? `- ${group.subject || '___'} -\n\n💠 *Group Description:*\n${
-                  group.desc
-              }\n\nHope you follow the rules and have fun!\n*‣ ${event.participants
+            ? `${group.desc}\n\n*‣ ${event.participants
                   .map((jid) => `@${jid.split('@')[0]}`)
                   .join(', ')}*`
             : event.action === 'remove'
-            ? `*@${event.participants[0].split('@')[0]}* has left the chat 👋`
+            ? `*@${event.participants[0].split('@')[0]}* Bye. We'll miss you 💜`
             : `*@${event.participants[0].split('@')[0]}* got ${this.client.util.capitalize(event.action)}d${
                   event.actor ? ` by *@${event.actor.split('@')[0]}*` : ''
               }`
