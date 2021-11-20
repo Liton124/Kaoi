563102f5734d61458e037d0076b284c3d4eb8839
Liton124 <92647972+Liton124@users.noreply.github.com>
2021-11-20 16:13:39 +0530

    Update Loli.ts

--- src/commands/Anime/Loli.ts
+++ src/commands/Anime/Loli.ts
@@ -13,7 +13,7 @@ export default class Command extends BaseCommand {
 			command: "loli",
 			description: `Will send you random loli image.`,
 			aliases: ["loli"],
-			category: "weeb",
+			category: "anime",
 			usage: `${client.config.prefix}loli `,
 			baseXp: 50,
 		});
