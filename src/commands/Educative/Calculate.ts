b74163dba6651b822ae9636dfcc4b6fdcb3296bb
Liton124 <92647972+Liton124@users.noreply.github.com>
2021-11-21 23:35:34 +0530

    Create Calculate.ts

---
+++ src/commands/Educative/Calculate.ts
@@ -0,0 +1,30 @@
+import WAClient from '../../lib/WAClient'
+import { IParsedArgs, ISimplifiedMessage } from '../../typings'
+import axios from 'axios'
+
+export default class Command extends BaseCommand {
+    constructor(client: WAClient, handler: MessageHandler) {
+        super(client, handler, {
+            command: 'calculate',
+            aliases: ['cal'],
+            description: 'Will solve the given question. ',
+            category: 'educative',
+            usage: `${client.config.prefix}calculator [question]`,
+            baseXp: 50
+        })
+    }
+
+    run = async (M: ISimplifiedMessage, { joined }: IParsedArgs): Promise<void> => {
+        if (!joined) return void M.reply('*Expressions:* \n2B = Addition(+)\n- = Subtraction(-)\n/ = Divide(÷)\n* = Multiply(×)\n')
+        const solve = joined.trim()
+        await axios.get(`https://api.mathjs.org/v4/?expr=(${solve})`)
+        .then((response) => {
+                // console.log(response);
+                const text = `💡 *Solution for ${solve} = ${response.data}*`
+                M.reply(text);
+            }).catch(err => {
+                M.reply(`✖ Invalid Expression.\nUse ${this.client.config.prefix}calc to see the expressions.`)
+            }
+            )
+    };
+}
