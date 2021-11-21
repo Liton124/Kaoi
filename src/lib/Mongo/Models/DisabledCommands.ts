8aa94bcb5bf1f8d3a89ffd88bec10ba532ad8022
Liton124 <92647972+Liton124@users.noreply.github.com>
2021-11-21 14:56:14 +0530

    Update DisabledCommands.ts

--- src/lib/Mongo/Models/DisabledCommands.ts
+++ src/lib/Mongo/Models/DisabledCommands.ts
@@ -5,7 +5,7 @@ const DisabledCommandSchema = new Schema({
     command: {
         type: String,
         unique: true,
-        required: false
+        required: true
     },
     reason: {
         type: String,
