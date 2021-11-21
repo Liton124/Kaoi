047d45dbe6cffb46b9ed635f44d2d0d516625909
Liton124 <92647972+Liton124@users.noreply.github.com>
2021-11-22 00:41:41 +0530

    Create Online.ts

---
+++ src/commands/General/Online.ts
@@ -0,0 +1,6 @@
+let id = args && /\d+\-\d+@g.us/.test(args[0]) ? args[0] : m.chat
+  let online = [...Object.keys(conn.chats.get(id).presences), conn.user.jid]
+  conn.reply(m.chat, 'List Online:\n' + online.map(v => '- @' + v.replace(/@.+/, '')).join`\n`, m, {
+    contextInfo: { mentionedJid: online }
+  })
+}
