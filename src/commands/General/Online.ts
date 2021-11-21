37727528bd3000877ad2c24d507afea69810179f
Liton124 <92647972+Liton124@users.noreply.github.com>
2021-11-22 00:43:13 +0530

    Update Online.ts

--- src/commands/General/Online.ts
+++ src/commands/General/Online.ts
@@ -1,3 +1,12 @@
+import MessageHandler from '../../Handlers/MessageHandler'
+import BaseCommand from '../../lib/BaseCommand'
+import WAClient from '../../lib/WAClient'
+import { ISimplifiedMessage } from '../../typings'
+
+export default class Command extends BaseCommand {
+    constructor(client: WAClient, handler: MessageHandler) {
+        super(client, handler, {
+
 let id = args && /\d+\-\d+@g.us/.test(args[0]) ? args[0] : m.chat
   let online = [...Object.keys(conn.chats.get(id).presences), conn.user.jid]
   conn.reply(m.chat, 'List Online:\n' + online.map(v => '- @' + v.replace(/@.+/, '')).join`\n`, m, {
