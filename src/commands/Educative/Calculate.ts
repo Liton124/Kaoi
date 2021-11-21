b2440d3a91861bfd4ddf59d5ca5a380077743d81
Liton124 <92647972+Liton124@users.noreply.github.com>
2021-11-21 23:36:57 +0530

    Update Calculate.ts

--- src/commands/Educative/Calculate.ts
+++ src/commands/Educative/Calculate.ts
@@ -1,3 +1,5 @@
+import MessageHandler from '../../Handlers/MessageHandler'
+import BaseCommand from '../../lib/BaseCommand'
 import WAClient from '../../lib/WAClient'
 import { IParsedArgs, ISimplifiedMessage } from '../../typings'
 import axios from 'axios'
