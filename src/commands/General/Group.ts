c0a0303c84be52c05f3e4e55a29532b9cc4469c1
Liton124 <92647972+Liton124@users.noreply.github.com>
2021-11-23 01:21:08 +0530

    Update Group.ts

--- src/commands/General/Group.ts
+++ src/commands/General/Group.ts
@@ -17,17 +17,17 @@ export default class Command extends BaseCommand {
         }
         run = async (M: ISimplifiedMessage): Promise<void> => {
             const owner = this.client.contacts[metadata.owner]
-            let text = `💮 *Title:* ${metadata.subject}\n\n👑 *Created By:* ${
+            let text = caption: `💮 *Title:* ${metadata.subject}\n\n👑 *Created By:* ${
                 owner?.notify || owner?.vname || owner?.name || metadata.owner.split('@')[0]
-               }\n\n📅 *Created On:* ${moment(metadata.creation * 1000).format('DD/MM HH:mm:ss')}\n\n🔊 *Announce:* ${
+            }\n\n📅 *Created On:* ${moment(metadata.creation * 1000).format('DD/MM HH:mm:ss')}\n\n🔊 *Announce:* ${
                 metadata.announce || false
-               }\n\n🍀 *Restricted:* ${metadata.restrict || metadata.restrict || false}\n\n🏊 *Participants:* ${
-                 metadata.participants.length
-               }\n\n🏅 *Admins:* ${
+            }\n\n🍀 *Restricted:* ${metadata.restrict || metadata.restrict || false}\n\n🏊 *Participants:* ${
+                metadata.participants.length
+            }\n\n🏅 *Admins:* ${
                 metadata.participants.filter((participant: { isAdmin: unknown }) => participant.isAdmin).length
-               }\n\n🎯 *Moderation:* ${mod}\n\n🔮 *Events:* ${events}\n\n🌟 *Safe:* ${safe}\n\n🔞 *NSFW:* ${nsfw}\n\n〽 *Description:* \n${
+            }\n\n🎯 *Moderation:* ${mod}\n\n🔮 *Events:* ${events}\n\n🌟 *Safe:* ${safe}\n\n🔞 *NSFW:* ${NSFW}\n\n〽 *Description:* \n${
                 metadata.desc
-            return void (await M.reply(text))
-        };
+            }`,
+            return void await(M.reply(text)),
+        }
     }
-}
