cc1efe094eb59e0e0a325d7cadc5ee9e0db368b8
Liton124 <92647972+Liton124@users.noreply.github.com>
2021-11-23 00:56:40 +0530

    Update Group.ts

--- src/commands/General/Group.ts
+++ src/commands/General/Group.ts
@@ -17,7 +17,7 @@ export default class Command extends BaseCommand {
         }
         const owner = this.client.contacts[metadata.owner]
         return {
-            body: icon ? await Utils.download(icon) : await readFile(join(this.client.assets, 'images', 'yui.jpg')),
+            
             caption: `💮 *Title:* ${metadata.subject}\n\n👑 *Created By:* ${
                 owner?.notify || owner?.vname || owner?.name || metadata.owner.split('@')[0]
             }\n\n📅 *Created On:* ${moment(metadata.creation * 1000).format('DD/MM HH:mm:ss')}\n\n🔊 *Announce:* ${
@@ -28,5 +28,7 @@ export default class Command extends BaseCommand {
                 metadata.participants.filter((participant: { isAdmin: unknown }) => participant.isAdmin).length
             }\n\n🎯 *Moderation:* ${mod}\n\n🔮 *Events:* ${events}\n\n🌟 *Safe:* ${safe}\n\n🔞 *NSFW:* ${NSFW}\n\n〽 *Description:* \n${
                 metadata.desc
-            
+        
+        };
 
+}
