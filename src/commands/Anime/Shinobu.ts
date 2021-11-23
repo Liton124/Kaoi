bdd960e841e9302cf0533d5dfe0e2cc327a8d151
Liton124 <92647972+Liton124@users.noreply.github.com>
2021-11-23 08:51:45 +0530

    Update Shinobu.ts

--- src/commands/Anime/Shinobu.ts
+++ src/commands/Anime/Shinobu.ts
@@ -13,7 +13,7 @@ export default class Command extends BaseCommand {
             command: 'shinobu',
             description: 'sends you a shinobu',
             aliases: ['shinobu'],
-            category: 'fun',
+            category: 'anime',
             usage: `${client.config.prefix}shinobu`
         })
     }
