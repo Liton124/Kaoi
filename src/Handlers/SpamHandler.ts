1b3ecfb40023b2346916057cb2e2424f14d02a4e
Liton124 <92647972+Liton124@users.noreply.github.com>
2021-11-23 22:22:59 +0530

    Update SpamHandler.ts

--- src/Handlers/SpamHandler.ts
+++ src/Handlers/SpamHandler.ts
@@ -2,10 +2,13 @@ import MessageHandler from "../../Handlers/MessageHandler";
 import BaseCommand from '../lib/BaseCommand'
 import WAClient from '../lib/WAClient'
 import { ISimplifiedMessage } from '../typings'
-       if (!M.message) return
-        this.spam = this.spam ? this.spam : {}
-        if (M.sender in this.spam) {
-            this.spam[M.sender].count++
+
+export default class EventHandler {
+    constructor(public client: WAClient) {}
+    if (!M.message) return
+    this.spam = this.spam ? this.spam : {}
+    if (M.sender in this.spam) {
+        this.spam[M.sender].count++
             if (M.messageTimestamp.toNumber() - this.spam[M.sender].lastspam > 20) {
                 if (this.spam[M.sender].count > 10) {
                     M.reply('*Jangan Spam!!*')
