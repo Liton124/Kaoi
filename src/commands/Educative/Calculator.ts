994508ca34e2a607fe33c9a18453641732f2fbaa
Liton124 <92647972+Liton124@users.noreply.github.com>
2021-11-23 10:43:43 +0530

    Update Calculator.ts

--- src/commands/Educative/Calculator.ts
+++ src/commands/Educative/Calculator.ts
@@ -22,7 +22,7 @@ export default class Command extends BaseCommand {
         await axios.get(`https://api.mathjs.org/v4/?expr=(${solve})`)
         .then((response) => {
                 // console.log(response);
-                const text = `💡 *Solution for [ ${solve} ] = ${response.data}*`
+                const text = `「 CALCULATOR 」\n\n${solve} = ${response.data}`
                 M.reply(text);
             }).catch(err => {
                 M.reply(`✖ Invalid Expression.\nUse ${this.client.config.prefix}calc to see the expressions.`)
