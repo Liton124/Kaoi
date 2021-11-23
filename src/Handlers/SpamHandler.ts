da79d1437967aa8e661d508a7bd92ec524f587c3
Liton124 <92647972+Liton124@users.noreply.github.com>
2021-11-23 23:05:49 +0530

    Update SpamHandler.ts

--- src/Handlers/SpamHandler.ts
+++ src/Handlers/SpamHandler.ts
@@ -1,26 +1,24 @@
 import MessageHandler from "../../Handlers/MessageHandler";
-import BaseCommand from '../lib/BaseCommand'
-import WAClient from '../lib/WAClient'
-import { ISimplifiedMessage } from '../typings'
-
-export default class SpamHandler {
-    constructor(public client: WAClient, handler: MessageHandler) {}
-    async all(m) {
-        if (!m.message) return
+import BaseCommand from '../lib/BaseCommand';
+import WAClient from '../lib/WAClient';
+import { ISimplifiedMessage } from '../typings'; {
+    }
+    constructor(public client: WAClient)
+    async await {
+        if (!M.message) return
         this.spam = this.spam ? this.spam : {}
-        if (m.sender in this.spam) {
-            this.spam[m.sender].count++
-            if (m.messageTimestamp.toNumber() - this.spam[m.sender].lastspam > 10) {
-                if (this.spam[m.sender].count > 10) {
-                    //global.db.data.users[m.sender].banned = true
-                    m.reply('*Jangan Spam!!*')
+        if (M.sender in this.spam) {
+            this.spam[M.sender].count++
+            if (M.messageTimestamp.toNumber() - this.spam[M.sender].lastspam > 10) {
+                if (this.spam[M.sender].count > 20) {
+                    //global.db.data.users[M.sender].banned = true
+                    M.reply('*Jangan Spam!!*')
                 }
-                this.spam[m.sender].count = 0
-                this.spam[m.sender].lastspam = m.messageTimestamp.toNumber()
-            }
-        }
-        else this.spam[m.sender] = {
-            jid: m.sender,
+                this.spam[M.sender].count = 0
+                this.spam[M.sender].lastspam = M.messageTimestamp.toNumber()
+            }        }
+        else this.spam[M.sender] = {
+            jid: M.sender,
             count: 0,
             lastspam: 0
         }
