884b4e0f60c0a8b9f8119189290167ba1c422288
Liton124 <92647972+Liton124@users.noreply.github.com>
2021-11-24 19:09:23 +0530

    Update Jail.ts

--- src/commands/Fun/Jail.ts
+++ src/commands/Fun/Jail.ts
@@ -22,10 +22,9 @@ export default class Command extends BaseCommand {
             : M.mentioned[0]
             ? this.client.getProfilePicture(M.mentioned[0])
             : this.client.getProfilePicture(M.quoted?.sender || M.sender.jid))
-            const { data } = await axios.get('https://some-random-api.ml/canvas/jail?avatar=${image}')
-            const buffer = await request.buffer(data.url).catch((e) => {
-                return void M.reply(e.message)
-            }
+        const { data } = await axios.get('https://some-random-api.ml/canvas/jail?avatar=${image}')
+        const buffer = await request.buffer(data.url).catch((e) => {
+            return void M.reply(e.message)
         }
     }
 }
