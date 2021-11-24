459cc56a4a57da14840cb32df244b414db94a10c
Liton124 <92647972+Liton124@users.noreply.github.com>
2021-11-24 16:51:06 +0530

    Update Help.ts

--- src/commands/General/Help.ts
+++ src/commands/General/Help.ts
@@ -24,12 +24,12 @@ export default class Command extends BaseCommand {
                 if (!command) continue
                 if (!info?.config?.category) continue
                 if (Object.keys(categories).includes(info.config.category)) categories[info.config.category].push(info)
-                else {n
+                else {
                     categories[info.config.category] = []
                     categories[info.config.category].push(info)
                 }
             }
-            let text = `👋🏻 (💙ω💙) Konichiwa! *${M.sender.username}*\n\nI'm Kaoi\nMy prefix is  "!"\n\n1. Use *listed commands* only which are given below\n2. *Don't call* bot to avoid blocking\n3. There's only one type user:*free*\n\n\`
+            let text = `👋🏻 (💙ω💙) Konichiwa! *${M.sender.username}*\n\nI'm Kaoi\nMy prefix is  "!"\n\n1. Use *listed commands* only which are given below\n2. *Don't call* bot to avoid blocking\n3. There's only one type user:*free*\n\n\n`
             const keys = Object.keys(categories).sort((a, b) => a.localeCompare(b))
             for (const key of keys)
                 text += `${this.emojis[keys.indexOf(key)]} *${this.client.util.capitalize(key)}*\n❐ \`\`\`${categories[
