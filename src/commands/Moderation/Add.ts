d1963d7210641d599164a78f2813b61bb427f718
Liton124 <92647972+Liton124@users.noreply.github.com>
2021-11-24 09:33:33 +0530

    Update Add.ts

--- src/commands/Moderation/Add.ts
+++ src/commands/Moderation/Add.ts
@@ -22,14 +22,15 @@ export default class Command extends BaseCommand {
             return void M.reply(`❌ Failed to ${this.config.command}, make me ADMIN first, baka`)
         if (M.quoted?.sender) M.mentioned.push(M.quoted.sender)
         if (!M.mentioned.length) return void M.reply(`Please enter the numbers you want to ${this.config.command}`)
-        text.split(',')
-      .map(v => v.replace(/[^0-9]/g, ''))
+        M.mentioned.forEach(async (user) => {
+            .map(v => v.replace(/[^0-9]/g, ''))
       .filter(v => v.length > 4 && v.length < 20 && !_participants.includes(v + '@s.whatsapp.net'))
       .map(async v => [
         v,
         await conn.isOnWhatsApp(v + '@s.whatsapp.net')
       ])
-  )).filter(v => v[1]).map(v => v[0] + '@c.us')
+            // const usr = this.client.contacts[user]
+            // const username = usr.notify || usr.vname || usr.name || user.split('@')[0]
             }
             // check if user is Bot
             else if (this.client.user.jid === user) {
