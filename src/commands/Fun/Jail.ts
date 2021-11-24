f61b3a878500c8d014b3debc75b69ec21fadd77a
Liton124 <92647972+Liton124@users.noreply.github.com>
2021-11-24 19:56:20 +0530

    Update Jail.ts

--- src/commands/Fun/Jail.ts
+++ src/commands/Fun/Jail.ts
@@ -1,9 +1,9 @@
+import { MessageType } from '@adiwajshing/baileys'
 import MessageHandler from '../../Handlers/MessageHandler'
 import BaseCommand from '../../lib/BaseCommand'
 import WAClient from '../../lib/WAClient'
 import { IParsedArgs, ISimplifiedMessage } from '../../typings'
 import axios from 'axios'
-
 export default class Command extends BaseCommand {
     constructor(client: WAClient, handler: MessageHandler) {
         super(client, handler, {
@@ -14,6 +14,7 @@ export default class Command extends BaseCommand {
             baseXp: 30
         })
     }
+
     run = async (M: ISimplifiedMessage, { joined }: IParsedArgs): Promise<void> => {
         const image = await (M.WAMessage?.message?.imageMessage
             ? this.client.downloadMediaMessage(M.WAMessage)
@@ -22,14 +23,14 @@ export default class Command extends BaseCommand {
             : M.mentioned[0]
             ? this.client.getProfilePicture(M.mentioned[0])
             : this.client.getProfilePicture(M.quoted?.sender || M.sender.jid))
-        await axios
-            .get('https://some-random-api.ml/canvas/jail?avatar=${image}')
+
+            await axios.get(`https://some-random-api.ml/canvas/jail?avatar=${image}`)
             .then((response) => {
-                // console.log(response); => {
-                    return void M.reply(response.data)
-                })
-            }
-        }
+                //console.log(response)
+                M.reply(response.data)
+            }).catch((e)=>{
+                M.reply('sorry couldn\'t send the image')
+            })
+
     }
 }
-
