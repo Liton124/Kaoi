9928c4d4df715fbe341843b73aaf4a02c25a7a71
Liton124 <92647972+Liton124@users.noreply.github.com>
2021-11-25 01:39:47 +0530

    Create Group.ts

---
+++ src/commands/General/Group.ts
@@ -0,0 +1,12 @@
+caption: `💮 *Title:* ${metadata.subject}\n\n👑 *Created By:* ${
+                owner?.notify || owner?.vname || owner?.name || metadata.owner.split('@')[0]
+            }\n\n📅 *Created On:* ${moment(metadata.creation * 1000).format('DD/MM HH:mm:ss')}\n\n🔊 *Announce:* ${
+                metadata.announce || false
+            }\n\n🍀 *Restricted:* ${metadata.restrict || metadata.restrict || false}\n\n🏊 *Participants:* ${
+                metadata.participants.length
+            }\n\n🏅 *Admins:* ${
+                metadata.participants.filter((participant: { isAdmin: unknown }) => participant.isAdmin).length
+            }\n\n🎯 *Moderation:* ${mod}\n\n🔮 *Events:* ${events}\n\n🌟 *Safe:* ${safe}\n\n🔞 *NSFW:* ${NSFW}\n\n〽 *Description:* \n${
+                metadata.desc
+            }`,
+            type: MessageType.image
