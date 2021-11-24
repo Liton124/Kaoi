271dc39540d6f51656af69dff95da3e391719cab
Liton124 <92647972+Liton124@users.noreply.github.com>
2021-11-24 16:35:34 +0530

    Update Wallpaper.ts

--- src/commands/Anime/Wallpaper.ts
+++ src/commands/Anime/Wallpaper.ts
@@ -15,7 +15,7 @@ export default class Command extends BaseCommand {
 			description: `Will send you random anime wallpaper of the given term.`,
 			aliases: ["wpaper", "wp"],
 			category: "anime",
-			usage: `${client.config.prefix}wallpaper [term]`,
+			usage: `${client.config.prefix}wallpaper [term,amount]`,
 			baseXp: 20,
 		});
 	}
@@ -26,12 +26,12 @@ export default class Command extends BaseCommand {
 	): Promise<void> => {
 		if (!joined)
 			return void (await M.reply(`Give me a wallpaper term to search, Baka!`));
-		const chitoge: any = joined.trim().split("|");
-		const term: string = chitoge[0];
-		const amount: number = chitoge[1];
+		const chitoge: any = joined.trim().split(",");
+		const term: string = kaoi[0];
+		const amount: number = kaoi[1];
 		if (!amount)
 			return void M.reply(
-				`Give me the number of wallpapers to send, Baka!\n\nExample: *${this.client.config.prefix}wallpaper chitoge|5*`
+				`Give me the number of wallpapers to send, Baka!\n\nExample: *${this.client.config.prefix}wallpaper BTS,5*`
 			);
 		if (amount > 20)
 			return void M.reply(`Do you want me to spam in this group?`);
