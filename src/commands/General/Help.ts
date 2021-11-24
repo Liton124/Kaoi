bea2ac8e3497edc7bb1fb1961e8417279d67cfa6
Liton124 <92647972+Liton124@users.noreply.github.com>
2021-11-24 16:47:30 +0530

    Update Help.ts

--- src/commands/General/Help.ts
+++ src/commands/General/Help.ts
@@ -29,7 +29,7 @@ export default class Command extends BaseCommand {
                     categories[info.config.category].push(info)
                 }
             }
-            let text = `👋🏻 (💙ω💙) Konichiwa! *${M.sender.username}*\n\nI'm Kaoi\nMy prefix is  "!"\n\n1. Use *listed commands* only which are given below\n2. *Don't call* bot to avoid blocking\n3. There's only one type user:*free*\n\n\n`
+            let text = `👋🏻 (💙ω💙) Konichiwa! *${M.sender.username}*\n\nI'm Kaoi\nMy prefix is  "!"\n\n1. Use *listed commands* only which are given below\n2. *Don't call* bot to avoid blocking\n3. There's only one type user:*free*\n\n\`
             const keys = Object.keys(categories).sort((a, b) => a.localeCompare(b))
             for (const key of keys)
                 text += `${this.emojis[keys.indexOf(key)]} *${this.client.util.capitalize(key)}*\n❐ \`\`\`${categories[
