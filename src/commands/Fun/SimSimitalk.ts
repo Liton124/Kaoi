0e389f3bf923c753ff6a33fc1661d39a4b64814d
Liton124 <92647972+Liton124@users.noreply.github.com>
2021-11-25 17:32:42 +0530

    Create SimSimitalk.ts

---
+++ src/commands/Fun/SimSimitalk.ts
@@ -0,0 +1,19 @@
+import MessageHandler from '../../Handlers/MessageHandler'
+import BaseCommand from '../../lib/BaseCommand'
+import WAClient from '../../lib/WAClient'
+import { ISimplifiedMessage } from '../../typings'
+import axios from 'axios'
+
+export default class Command extends BaseCommand {
+    constructor(client: WAClient, handler: MessageHandler) {
+        super(client, handler, {
+            command: 'simsimi',
+            description: 'Chat with the Bot in group',
+            aliases: ['simi'],
+            category: 'fun',
+            usage: `${client.config.prefix}bot (text)`,
+            baseXp: 30
+        })
+    }
+
+    run = async (M: ISimplifiedMessage): Promise<void> => {
