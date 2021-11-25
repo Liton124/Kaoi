8c7c240a8cea15aedaeece68a4801dc098c3523f
Liton124 <92647972+Liton124@users.noreply.github.com>
2021-11-25 13:09:32 +0530

    Create Dadjoke.ts

---
+++ src/commands/Fun/Dadjoke.ts
@@ -0,0 +1,31 @@
+import MessageHandler from '../../Handlers/MessageHandler'
+import BaseCommand from '../../lib/BaseCommand'
+import WAClient from '../../lib/WAClient'
+import { ISimplifiedMessage } from '../../typings'
+import axios from 'axios'
+
+export default class Command extends BaseCommand {
+    constructor(client: WAClient, handler: MessageHandler) {
+        super(client, handler, {
+            command: 'dadjoke',
+            aliases: ['djoke'],
+            description: 'Gives you a dadjoke',
+            category: 'fun',
+            usage: `${client.config.prefix}dadjoke`,
+            baseXp: 30
+        })
+    }
+
+    run = async (M: ISimplifiedMessage): Promise<void> => {
+        await axios
+            .get(`https://icanhazdadjoke.com/slack`)
+            .then((response) => {
+                // console.log(response);
+                const text = `*First person:* ${response.data.fallback}\n\n*Second person:* ${response.data.text}`
+                M.reply(text)
+            })
+            .catch((err) => {
+                M.reply(`🔍 Error: ${err}`)
+            })
+    }
+}
