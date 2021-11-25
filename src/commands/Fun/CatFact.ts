5cf389ec99ffd16fb288ae371465141e5dcf410d
Liton124 <92647972+Liton124@users.noreply.github.com>
2021-11-25 14:16:50 +0530

    Create CatFact.ts

---
+++ src/commands/Fun/CatFact.ts
@@ -0,0 +1,31 @@
+import MessageHandler from '../../Handlers/MessageHandler'
+import BaseCommand from '../../lib/BaseCommand'
+import WAClient from '../../lib/WAClient'
+import { ISimplifiedMessage } from '../../typings'
+import axios from 'axios'
+
+export default class Command extends BaseCommand {
+    constructor(client: WAClient, handler: MessageHandler) {
+        super(client, handler, {
+            command: 'catfact',
+            aliases: ['catfact'],
+            description: 'Gives you a random cat fact',
+            category: 'fun',
+            usage: `${client.config.prefix}catfact`,
+            baseXp: 10
+        })
+    }
+
+        run = async (M: ISimplifiedMessage): Promise<void> => {
+        await axios
+            .get(`https://some-random-api.ml/facts/cat`)
+            .then((response) => {
+                // console.log(response); 
+                const text = `    🐈「 FACT 」🐈\n\n${response.data.fact}`
+                M.reply(text)
+            })
+            .catch((err) => {
+                M.reply(`🔍 Error: ${err}`)
+            })
+    }
+}
