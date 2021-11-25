49308e7b67acd8f7d1b1cbec44f05100b74017d2
Liton124 <92647972+Liton124@users.noreply.github.com>
2021-11-25 20:46:56 +0530

    Create Add.ts

---
+++ src/commands/Moderation/Add.ts
@@ -0,0 +1,25 @@
+import MessageHandler from '../../Handlers/MessageHandler'
+import BaseCommand from '../../lib/BaseCommand'
+import WAClient from '../../lib/WAClient'
+import { ISimplifiedMessage } from '../../typings'
+
+export default class Command extends BaseCommand {
+    constructor(client: WAClient, handler: MessageHandler) {
+        super(client, handler, {
+            adminOnly: true,
+            aliases: ['boom', 'kickout'],
+            command: 'add',
+            description: 'removes the mentioned users',
+            category: 'moderation',
+            usage: `${client.config.prefix}remove [@mention | tag]`,
+            baseXp: 10
+        })
+    }
+
+    run = async (M: ISimplifiedMessage): Promise<void> => {
+        let text = '*Action*\n\n'
+        if (!M.groupMetadata?.admins?.includes(this.client.user.jid))
+            return void M.reply(`❌ Failed to ${this.config.command}\nMake me admin first, Baka`)
+        if (M.quoted?.sender) M.mentioned.push(M.quoted.sender)
+        if (!M.mentioned.length) return void M.reply(`please enter the numbers you want to add`)
+        M.mentioned.forEach(async (user) => {
