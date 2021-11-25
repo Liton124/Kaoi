8d659114707763d1d451e702879e811fa9ec6854
Liton124 <92647972+Liton124@users.noreply.github.com>
2021-11-25 17:34:57 +0530

    Update SimSimitalk.ts

--- src/commands/Fun/SimSimitalk.ts
+++ src/commands/Fun/SimSimitalk.ts
@@ -17,3 +17,14 @@ export default class Command extends BaseCommand {
     }
 
     run = async (M: ISimplifiedMessage): Promise<void> => {
+        await axios
+                .get(
+                    `${encodeURI(
+                        `&uid=${
+                            M.from
+                        }&msg=${M.args.slice(1)}`
+                    )}`
+                )
+                .then((res) => {
+                    if (res.status !== 200) return void M.reply(`🔍 Error: ${res.status}`)
+                    return void M.reply(res.data.cnt)
