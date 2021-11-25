7ca920f7c5b26b7460d5a506d56513740e6debf2
Liton124 <92647972+Liton124@users.noreply.github.com>
2021-11-25 11:43:32 +0530

    Create Dog.ts

---
+++ src/commands/Fun/Dog.ts
@@ -0,0 +1,31 @@
+import MessageHandler from '../../Handlers/MessageHandler'
+import BaseCommand from '../../lib/BaseCommand'
+import WAClient from '../../lib/WAClient'
+import { ISimplifiedMessage } from '../../typings'
+import axios from 'axios'
+
+export default class Command extends BaseCommand {
+    constructor(client: WAClient, handler: MessageHandler) {
+        super(client, handler, {
+            command: 'dog',
+            aliases: ['doggy],
+            description: 'Gives you random advice.\nDisclaimer: We do not hold responsibility of consequences of your actions based on the advice.',
+            category: 'fun',
+            usage: `${client.config.prefix}advice`,
+            baseXp: 30
+        })
+    }
+
+    run = async (M: ISimplifiedMessage): Promise<void> => {
+        await axios
+            .get(`https://some-random-api.ml/animal/dog`)
+            .then((response) => {
+                // console.log(response);
+                const text = `image: ${image}, fact: ${fact}`
+                M.reply(text)
+            })
+            .catch((err) => {
+                M.reply(`🔍 Error: ${err}`)
+            })
+    }
+}
