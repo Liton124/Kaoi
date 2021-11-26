77602d108a04f1d060bd78be9cda3035bacd767a
Liton124 <92647972+Liton124@users.noreply.github.com>
2021-11-26 16:03:40 +0530

    Update Calculator.ts

--- src/commands/Educative/Calculator.ts
+++ src/commands/Educative/Calculator.ts
@@ -22,7 +22,7 @@ export default class Command extends BaseCommand {
         if (!joined) return void M.reply('Provide the value to calculate, Baka!')
         const value = joined.trim()
         const calc = evaluate(value);
-				const text = ` 「 CALCULATOR 」\n\n *📝 Given Value:* ${value} \n\n *💡 Solution:* ${calc}\n`;
+				const text = ` 「 CALCULATOR 」\n\n*${value}=* *${response.data.result}*\n  `;
         await M.reply(text)
             // eslint-disable-next-line @typescript-eslint/no-explicit-any
             .catch((reason: any) => M.reply(`${reason}`))
