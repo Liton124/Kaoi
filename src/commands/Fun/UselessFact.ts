cf25eff2e9a23e0907158be2b2b6b20dc10a46e8
Liton124 <92647972+Liton124@users.noreply.github.com>
2021-11-26 17:44:18 +0530

    Update UselessFact.ts

--- src/commands/Fun/UselessFact.ts
+++ src/commands/Fun/UselessFact.ts
@@ -21,7 +21,7 @@ export default class Command extends BaseCommand {
             .get(`https://uselessfacts.jsph.pl/random.json?language=en`)
             .then((response) => {
                 // console.log(response);
-                const text = `🗿 *FACT:* ${responce.data.text}`
+                const text = `🗿 *FACT:* ${response.data.text}`
                 M.reply(text)
             })
             .catch((err) => {
