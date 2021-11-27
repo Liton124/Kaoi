02d0b6a0f4c45cc943828585f286fbf5fb245d4d
Liton124 <92647972+Liton124@users.noreply.github.com>
2021-11-27 14:04:39 +0530

    Update Image.ts

--- src/commands/Media/Image.ts
+++ src/commands/Media/Image.ts
@@ -1,51 +1,42 @@
-import MessageHandler from '../../Handlers/MessageHandler'
-import BaseCommand from '../../lib/BaseCommand'
-import WAClient from '../../lib/WAClient'
-import { IParsedArgs, ISimplifiedMessage } from '../../typings'
-import axios from 'axios'
-import request from '../../lib/request'
-import { MessageType } from '@adiwajshing/baileys'
+import MessageHandler from "../../Handlers/MessageHandler";
+import BaseCommand from "../../lib/BaseCommand";
+import WAClient from "../../lib/WAClient";
+import { IParsedArgs, ISimplifiedMessage } from "../../typings";
+import request from "../../lib/request";
+import { MessageType, Mimetype } from "@adiwajshing/baileys";
+import googleImage from "@marsron/google-image";
 
 export default class Command extends BaseCommand {
-    constructor(client: WAClient, handler: MessageHandler) {
-        super(client, handler, {
-            command: 'image',
-            description: 'Will search img from the given term',
-            aliases: ['img'],
-            category: 'media',
-            usage: `${client.config.prefix}img`
-        })
-    }
+	constructor(client: WAClient, handler: MessageHandler) {
+		super(client, handler, {
+			command: "goimage",
+			description: `Will search the given image from google.`,
+			aliases: ["goimg", "googleimg", "googleimage"],
+			category: "media",
+			usage: `${client.config.prefix}goimg [term|amount]`,
+			baseXp: 50,
+		});
+	}
 
-    run = async (M: ISimplifiedMessage, { joined }: IParsedArgs): Promise<void> => {
-        if (!joined) return void (await M.reply(`Please give a term to search`));
-        const search: any = joined.trim();
-        const term: string = search;
-        const { data } = await axios.get('https://imsea.herokuapp.com/api/1?q=${term}');
-        const buffer = await request.buffer(data.results).catch((e) => {
-            return void M.reply(e.message)
-        })
-        while (true) {
-            try {
-                M.reply(
-                    buffer || 'Could not fetch image. Please try again later',
-                    MessageType.image,
-                    undefined,
-                    undefined,
-                    `HERE YOU GO ✨`,
-                    undefined
-                ).catch((e) => {
-                    console.log(`This error occurs when an image is sent via M.reply()\n Child Catch Block : \n${e}`)
-                    // console.log('Failed')
-                    M.reply(`Try again or use the link. Here's the URL: ${data.results}`)
-                })
-                break
-            } catch (e) {
-                // console.log('Failed2')
-                M.reply(`Try again or use the link. Here's the URL : ${data.results}`)
-                console.log(`This error occurs when an image is sent via M.reply()\n Parent Catch Block : \n${e}`)
-            }
-        }
-        return void null
-    }
+	run = async (
+		M: ISimplifiedMessage,
+		{ joined }: IParsedArgs
+	): Promise<void> => {
+                if (!joined) return void M.reply('Give a search term,baka!')
+		const search: any = joined.trim().split("|");
+		const term: string = search[0];
+		const amount: number = search[1];
+		if (amount > 20)
+			return void M.reply(`Do you want me to spam in this group?`);
+		const img = await googleImage(term);
+		for (let i = 0; i < amount; i++) {
+			const res = `*🌟 Here you go.*`;
+			this.client.sendMessage(M.from, { url: img[i].url }, MessageType.image, {
+				quoted: M.WAMessage,
+				mimetype: Mimetype.png,
+				caption: `${res}`,
+			});
+			await M.reply(`Wait..`);
+		}
+	};
 }
