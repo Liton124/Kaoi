580db5db6f4a9e83256cda97b90511081e5172be
Liton124 <92647972+Liton124@users.noreply.github.com>
2021-11-27 11:41:27 +0530

    Update DogFact.ts

--- src/commands/Fun/DogFact.ts
+++ src/commands/Fun/DogFact.ts
@@ -31,7 +31,7 @@ export default class Command extends BaseCommand {
                     MessageType.image,
                     undefined,
                     undefined,
-                    `🐶「 FACT 」🐶\n\n${text}\n`,
+                    `\n${text}\n`,
                     undefined
                 ).catch((e) => {
                     console.log(`This error occurs when an image is sent via M.reply()\n Child Catch Block : \n${e}`)
