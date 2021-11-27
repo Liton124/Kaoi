d50f242121985971448a2e55c405a368e717fb5f
Liton124 <92647972+Liton124@users.noreply.github.com>
2021-11-27 12:39:55 +0530

    Update Image.ts

--- src/commands/Fun/Image.ts
+++ src/commands/Fun/Image.ts
@@ -23,32 +23,32 @@ export default class Command extends BaseCommand {
 	): Promise<void> => {
 		if (!joined)
 			return void (await M.reply(`Give me a search term, Baka!`));
-        const search: any = joined.trim();
-        const term: string = search;
-        const { data } = await axios.get('https://imsea.herokuapp.com/api/1?q=${term}')
-        const buffer = await request.buffer(data.results).catch((e) => {
-            return void M.reply(e.message)
-        })
-        while (true) {
-            try {
-                M.reply(
-                    buffer || 'Could not fetch image. Please try again later',
-                    MessageType.image,
-                    undefined,
-                    undefined,
-                    `Here you go`,
-                    undefined
-                ).catch((e) => {
-                    console.log(`This error occurs when an image is sent via M.reply()\n Child Catch Block : \n${e}`)
-                    // console.log('Failed')
-                    M.reply(`Try again or use the link. Here's the URL: ${data.results}`)
+                const search: any = joined.trim();
+                const term: string = search;
+                const { data } = await axios.get('https://imsea.herokuapp.com/api/1?q=${term}')
+                const buffer = await request.buffer(data.results).catch((e) => {
+                    return void M.reply(e.message)
                 })
-                break
-            } catch (e) {
-                // console.log('Failed2')
-                M.reply(`Try again or use the link. Here's the URL : ${data.image}`)
-                console.log(`This error occurs when an image is sent via M.reply()\n Parent Catch Block : \n${e}`)
-            }
+                while (true) {
+                    try {
+                        M.reply(
+                            buffer || 'Could not fetch image. Please try again later',
+                            MessageType.image,
+                            undefined,
+                            undefined,
+                           `Here you go`,
+                            undefined
+                        ).catch((e) => {
+                            console.log(`This error occurs when an image is sent via M.reply()\n Child Catch Block : \n${e}`)
+                            // console.log('Failed')
+                            M.reply(`Try again or use the link. Here's the URL: ${data.results}`)
+                    })
+                    break
+                } catch (e) {
+                    // console.log('Failed2')
+                    M.reply(`Try again or use the link. Here's the URL : ${data.image}`)
+                    console.log(`This error occurs when an image is sent via M.reply()\n Parent Catch Block : \n${e}`)
+                }
         }
         return void null
     }
