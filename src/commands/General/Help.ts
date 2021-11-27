ac3873b644280708512b74c1a8705eec13aaa8ae
Liton124 <92647972+Liton124@users.noreply.github.com>
2021-11-27 21:28:38 +0530

    Update Help.ts

--- src/commands/General/Help.ts
+++ src/commands/General/Help.ts
@@ -32,7 +32,7 @@ export default class Command extends BaseCommand {
             let text = `👋🏻 (💙ω💙) Konichiwa! *${M.sender.username}*\n\nI'm Kaoi 🤖\nMy prefix is "!"\n\n*📮Notes*\n 1. Use *listed commands* only which are given below.\n 2. *Don't call* bot to avoid blocking.\n 3. There's only *free* type user.\n\n\n`		
             const keys = Object.keys(categories).sort((a, b) => a.localeCompare(b))
             for (const key of keys)
-                text += `*━━━❰${this.emojis[keys.indexOf(key)]${this.client.util.capitalize(
+                text += `*━━━❰ ${this.emojis[keys.indexOf(key)] ${this.client.util.capitalize(
 					key
 				)} ❱━━━*\n❐ \`\`\`${categories[key]
                     .map((command) => command.config?.command)
