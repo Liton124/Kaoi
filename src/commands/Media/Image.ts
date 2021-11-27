8f29e9d6f8c4146ed36dd2821695d738250292df
Liton124 <92647972+Liton124@users.noreply.github.com>
2021-11-27 14:29:14 +0530

    Update Image.ts

--- src/commands/Media/Image.ts
+++ src/commands/Media/Image.ts
@@ -21,21 +21,22 @@ export default class Command extends BaseCommand {
 		M: ISimplifiedMessage,
 		{ joined }: IParsedArgs
 	): Promise<void> => {
-                if (!joined) return void M.reply('Give a search term,baka!')
-		const search: any = joined.trim().split("|");
-		const term: string = search[0];
-		const amount: number = search[1];
-		if (amount > 20)
-			return void M.reply(`Do you want me to spam in this group?`);
-		const img = await axios.get('https://imsea.herokuapp.com/api/1?q=${term}');
-		for (let i = 0; i < amount; i++) {
-			const res = `*🌟 Here you go.*`;
-			this.client.sendMessage(M.from, { url: img[i].url }, MessageType.image, {
+                    if (!joined) return void M.reply('Give a search term,baka!')
+		    const search: any = joined.trim().split("|");
+		    const term: string = search[0];
+		    const amount: number = search[1];
+		    if (amount > 20)
+                    return void M.reply(`Do you want me to spam in this group?`);
+		    const img = await axios.get('https://imsea.herokuapp.com/api/1?q=${term}');
+		    for (let i = 0; i < amount; i++) {
+                        const res = `*🌟 Here you go.*`;
+		        this.client.sendMessage(M.from, { url: img[i].url }, MessageType.image, {
 				quoted: M.WAMessage,
 				mimetype: Mimetype.jpg,
 				caption: `${res}`,
 			});
 			await M.reply(`Wait..`);
-		}
+		    }
+              }
 	};
 }
