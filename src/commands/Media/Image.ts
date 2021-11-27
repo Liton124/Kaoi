b8d701775e36b049a74a9fb576b498b11373dd37
Liton124 <92647972+Liton124@users.noreply.github.com>
2021-11-27 10:46:36 +0530

    Update Image.ts

--- src/commands/Media/Image.ts
+++ src/commands/Media/Image.ts
@@ -1,35 +1,48 @@
-import MessageHandler from "../../Handlers/MessageHandler";
-import BaseCommand from "../../lib/BaseCommand";
-import WAClient from "../../lib/WAClient";
-import { IParsedArgs, ISimplifiedMessage } from "../../typings";
-import request from "../../lib/request";
-import { MessageType, Mimetype } from "@adiwajshing/baileys";
+import MessageHandler from '../../Handlers/MessageHandler'
+import BaseCommand from '../../lib/BaseCommand'
+import WAClient from '../../lib/WAClient'
+import { ISimplifiedMessage } from '../../typings'
+import axios from 'axios'
+import request from '../../lib/request'
+import { MessageType } from '@adiwajshing/baileys'
 
 export default class Command extends BaseCommand {
-	constructor(client: WAClient, handler: MessageHandler) {
-		super(client, handler, {
-			command: "image",
-			description: `Will search the given image from google.`,
-			aliases: ["img"],
-			category: "media",
-			usage: `${client.config.prefix}img [term]`,
-			baseXp: 30,
-		});
-	}
+    constructor(client: WAClient, handler: MessageHandler) {
+        super(client, handler, {
+            command: 'meme',
+            description: 'Will send you a Meme.',
+            aliases: ['LOL'],
+            category: 'fun',
+            usage: `${client.config.prefix}meme`
+        })
+    }
 
-	run = async (
-		M: ISimplifiedMessage,
-		{ joined }: IParsedArgs
-	): Promise<void> => {
-		const search: any = joined.trim();
-		const term: string = search[0];
-		const img = await (`https://imsea.herokuapp.com/api/1?q=${term}`);
-
-		const res = `*🌟 Here you go.*`;
-		this.client.sendMessage(M.from, { url: img.url }, MessageType.image, {
-	                quoted: M.WAMessage,	
-                        mimetype: Mimetype.png,
-	                caption: `${res}`,
-		});
-	}
-}
+    run = async (M: ISimplifiedMessage): Promise<void> => {
+        // fetch result of https://zxbott.herokuapp.com/husbu from the API using axios
+        const { data } = await axios.get('https://some-random-api.ml/meme')
+        const buffer = await request.buffer(data.image).catch((e) => {
+            return void M.reply(e.message)
+        })
+        while (true) {
+            try {
+                M.reply(
+                    buffer || 'Could not fetch image. Please try again later',
+                    MessageType.image,
+                    undefined,
+                    undefined,
+                    `LOL  😂\n`,
+                    undefined
+                ).catch((e) => {
+                    console.log(`This error occurs when an image is sent via M.reply()\n Child Catch Block : \n${e}`)
+                    // console.log('Failed')
+                    M.reply(`Try again or use the link. Here's the URL: ${data.image}`)
+                })
+                break
+            } catch (e) {
+                // console.log('Failed2')
+                M.reply(`Try again or use the link. Here's the URL : ${data.image}`)
+                console.log(`This error occurs when an image is sent via M.reply()\n Parent Catch Block : \n${e}`)
+            }
+        }
+        return void null
+    }
