75db71de27686c9345e3e828a5d289634def7916
Liton124 <92647972+Liton124@users.noreply.github.com>
2021-12-01 14:24:29 +0530

    Update RIP.ts

--- src/commands/Fun/RIP.ts
+++ src/commands/Fun/RIP.ts
@@ -25,7 +25,7 @@ export default class Command extends BaseCommand {
 			? this.client.getProfilePicture(M.mentioned[0])
 			: this.client.getProfilePicture(M.quoted?.sender || M.sender.jid));
 		if (!image) return void M.reply(`Couldn't fetch the required Image`);
-		const result = await new DIG.Rip().getimage(image);
+		const result = await new DIG.Rip().getImage(image);
 		await M.reply(
 			result,
 			MessageType.image,
