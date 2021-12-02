77976654a3ff93f0a79c96a775b45d2c4994e5b5
Liton124 <92647972+Liton124@users.noreply.github.com>
2021-12-02 23:50:07 +0530

    Update Meme.ts

--- src/commands/Fun/Meme.ts
+++ src/commands/Fun/Meme.ts
@@ -30,7 +30,7 @@ export default class Command extends BaseCommand {
                     MessageType.image,
                     undefined,
                     undefined,
-                    `LOL  😂\n`,
+                    `LOL 😂`,
                     undefined
                 ).catch((e) => {
                     console.log(`This error occurs when an image is sent via M.reply()\n Child Catch Block : \n${e}`)
