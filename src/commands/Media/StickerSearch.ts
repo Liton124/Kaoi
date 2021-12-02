f27949cbefa6c9b67b84c5cc9005b233775246e3
Liton124 <92647972+Liton124@users.noreply.github.com>
2021-12-02 23:06:08 +0530

    Update StickerSearch.ts

--- src/commands/Media/StickerSearch.ts
+++ src/commands/Media/StickerSearch.ts
@@ -10,8 +10,8 @@ import { Sticker, Categories, StickerTypes } from 'wa-sticker-formatter'
 export default class Command extends BaseCommand {
     constructor(client: WAClient, handler: MessageHandler) {
         super(client, handler, {
-            command: 'stickerserach',
-            aliases: ['ssearch', 'ssh'],
+            command: 'stickersearch',
+            aliases: ['ssearch', 'ssch', 'ssh'],
             description: 'Search sticker. ',
             category: 'media',
             dm: true,
@@ -20,16 +20,16 @@ export default class Command extends BaseCommand {
     }
     // static count = 0
     run = async (M: ISimplifiedMessage, { joined }: IParsedArgs): Promise<void> => {
-
+        
         if (!joined) return void M.reply('Provide the keywords you wanna search, Baka!')
         const cara = joined.trim()
         console.log(cara)
-
-        const { data } = await axios.get(`https://api.xteam.xyz/sticker/stickerly?q=${cara}&APIKEY=80ff35f542ee4149`)
-
+        
+        const { data } = await axios.get(`https://api.mojilala.com/v1/stickers/search?q=${cara}&api_key=dc6zaTOxFJmzC`)
+        
 if ((data as { error: string }).error) return void (await M.reply('Sorry, couldn\'t find'))
-        const i = Math.floor(Math.random() * data.result.stickerlist.length)
-const b = `${data.result.stickerlist[i]}`
+        const i = Math.floor(Math.random() * data.data[0].id.images.fixed_height.length)
+const b = `${data.data[0].id.images.fixed_height[i].url}`
 
         const sticker: any = await new Sticker(b, {
 			pack: "sticker",
