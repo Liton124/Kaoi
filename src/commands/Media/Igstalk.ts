ba002cfc50b1e76557a02dcdbcbb90b82bd2e37d
Liton124 <92647972+Liton124@users.noreply.github.com>
2021-12-04 22:35:32 +0530

    Update Igstalk.ts

--- src/commands/Media/Igstalk.ts
+++ src/commands/Media/Igstalk.ts
@@ -29,7 +29,7 @@ export default class Command extends BaseCommand {
                           cookie: igCookie,
                           'user-agent':
                           'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.190 Safari/537.36',
-                    };
+                    },
                 })
                 .then(({data}) => {
                      const main = data.graphql.user;
