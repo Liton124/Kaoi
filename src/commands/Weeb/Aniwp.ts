262488427047ec2e795381740155d4504db5e471
Liton124 <92647972+Liton124@users.noreply.github.com>
2021-12-07 18:46:33 +0530

    Update and rename Awp.ts to Aniwp.ts

--- src/commands/Weeb/Aniwp.ts
+++ src/commands/Weeb/Aniwp.ts
@@ -1,5 +1,3 @@
-** @format */
-
 import { AnimeWallpaper } from "anime-wallpaper";
 import MessageHandler from "../../Handlers/MessageHandler";
 import BaseCommand from "../../lib/BaseCommand";
@@ -7,6 +5,7 @@ import WAClient from "../../lib/WAClient";
 import { IParsedArgs, ISimplifiedMessage } from "../../typings";
 import request from "../../lib/request";
 import { MessageType } from "@adiwajshing/baileys";
+
 export default class Command extends BaseCommand {
 	constructor(client: WAClient, handler: MessageHandler) {
 		super(client, handler, {
