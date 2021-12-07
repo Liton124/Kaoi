5d9227cc0faeade9e87a0c779bc9437370d5f72e
Liton124 <92647972+Liton124@users.noreply.github.com>
2021-12-07 12:23:55 +0530

    Update and rename src/commands/Anime/Waifu.ts to src/commands/Weeb/Waifu.ts

--- src/commands/Weeb/Waifu.ts
+++ src/commands/Weeb/Waifu.ts
@@ -13,7 +13,7 @@ export default class Command extends BaseCommand {
             command: 'waifu',
             description: `Will send you random waifu image.`,
             aliases: ['wife'],
-            category: 'anime',
+            category: 'weeb',
             usage: `${client.config.prefix}waifu`,
             baseXp: 50
         })
