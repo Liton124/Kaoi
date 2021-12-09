c70faff25f44c34986b37a609bb5c92fe79ffb1a
Liton124 <92647972+Liton124@users.noreply.github.com>
2021-12-09 23:21:51 +0530

    Update Chat.ts

--- src/commands/General/Chat.ts
+++ src/commands/General/Chat.ts
@@ -9,7 +9,7 @@ export default class Command extends BaseCommand {
         super(client, handler, {
             command: 'chat',
             description: 'Chat with the Bot in group',
-            aliases: ['/'],
+            aliases: ['chat'],
             category: 'general',
             usage: `${client.config.prefix}bot (text)`,
             baseXp: 30
