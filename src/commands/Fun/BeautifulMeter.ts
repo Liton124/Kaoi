667e69479bd9cbef51d1f79a2a30871493e992b0
Liton124 <92647972+Liton124@users.noreply.github.com>
2021-12-11 22:47:59 +0530

    Create BeautifulMeter.ts

---
+++ src/commands/Fun/BeautifulMeter.ts
@@ -0,0 +1,30 @@
+import MessageHandler from '../../Handlers/MessageHandler'
+import BaseCommand from '../../lib/BaseCommand'
+import WAClient from '../../lib/WAClient'
+import { ISimplifiedMessage } from '../../typings'
+
+export default class Command extends BaseCommand {
+    constructor(client: WAClient, handler: MessageHandler) {
+        super(client, handler, {
+            command: 'beautifulcheck',
+            description: 'Check anyone's beautifulness',
+            category: 'fun',
+            aliases: ['beautifulmeter', 'howbeautiful'],
+            usage: `${client.config.prefix}beautifulcheck [tag or quote] `,
+            baseXp: 0
+        })
+    }
+
+    run = async (M: ISimplifiedMessage): Promise<void> => {
+        let text = '_BEAUTIFULCHECK_ '
+        if (M.quoted?.sender) M.mentioned.push(M.quoted.sender)
+        if (!M.mentioned.length) return void M.reply(`Please tag *user* or *user message* for checking beautifulness`)
+        M.mentioned.forEach(async (user) => {
+            // const usr = this.client.contacts[user]
+            // const username = usr.notify || usr.vname || usr.name || user.split('@')[0]
+       
+            text += `for *@${user.split('@')[0]}*\n\n*@${user.split('@')[0]}* is *${Math.floor(Math.random() * 101)}%* _BEAUTIFUL_ 👽`             
+        })
+        await M.reply(`${text}`, undefined, undefined, [...M.mentioned, M.sender.jid])
+    }
+}
