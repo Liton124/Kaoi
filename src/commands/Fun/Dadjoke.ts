51856bf160639d1a896930d4d6948667d06d876f
Liton124 <92647972+Liton124@users.noreply.github.com>
2021-12-19 16:46:21 +0530

    Update Dadjoke.ts

--- src/commands/Fun/Dadjoke.ts
+++ src/commands/Fun/Dadjoke.ts
@@ -21,7 +21,7 @@ export default class Command extends BaseCommand {
             .get(`https://icanhazdadjoke.com/slack`)
             .then((response) => {
                 // console.log(response);
-                const text = `*dadjoke:* ${response.data.attachments[1].fallback}`
+                const text = `*dadjoke:* ${response.data.attachments[0].text}`
                 M.reply(text)
             })
             .catch((err) => {
