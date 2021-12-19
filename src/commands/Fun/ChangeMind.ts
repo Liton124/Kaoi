0fe58b0288d58a88c324a4a0f342f5ea8ab08503
Liton124 <92647972+Liton124@users.noreply.github.com>
2021-12-19 16:42:42 +0530

    Update ChangeMind.ts

--- src/commands/Fun/ChangeMind.ts
+++ src/commands/Fun/ChangeMind.ts
@@ -20,7 +20,7 @@ export default class Command extends BaseCommand {
     }
 
     run = async (M: ISimplifiedMessage, { joined }: IParsedArgs): Promise<void> => {
-        // consider neko and kitsune in furry
+        if(!joined) 
         
         const term = joined.trim()
 
