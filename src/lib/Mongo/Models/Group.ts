00cc5e53195aa35e0511cc441663c92d4e798a17
Liton124 <92647972+Liton124@users.noreply.github.com>
2022-02-24 15:40:37 +0530

    Update Group.ts

--- src/lib/Mongo/Models/Group.ts
+++ src/lib/Mongo/Models/Group.ts
@@ -51,6 +51,108 @@ const GroupSchema = new Schema({
         default: false
 
     },
+    chara: {
+    type: Boolean,
+    required: false,
+    default: false,
+  },
+
+  bot: {
+    type: String,
+    required: false,
+    default: "all",
+  },
+  wild: {
+    type: Boolean,
+    required: false,
+    default: false,
+  },
+  lastPokemon: {
+    type: String,
+    required: false,
+  },
+
+  pId: {
+    type: Number,
+    required: false,
+  },
+
+  pLevel: {
+    type: Number,
+    required: false,
+  },
+
+  pImage: {
+    type: String,
+    required: false,
+  },
+
+  catchable: {
+    type: Boolean,
+    required: false,
+    default: false,
+  },
+  trade: {
+    type: Boolean,
+  },
+  startedBy: {
+    type: String,
+  },
+  tOffer: {
+    name: {
+      type: String,
+    },
+    id: {
+      type: Number,
+    },
+    level: {
+      type: Number,
+    },
+    image: {
+      type: String,
+    },
+  },
+
+  tWant: {
+    type: String,
+  },
+
+  haigushaResponse: {
+    name: String,
+    id: Number,
+    claimable: Boolean,
+  },
+  quizResponse: {
+    id: Number,
+    answer: Number,
+    ongoing: Boolean,
+    startedBy: String,
+  },
+  charaResponse: {
+    id: Number,
+    name: String,
+    image: String,
+    claimable: Boolean,
+    price: Number,
+    about: String,
+    source: String,
+  },
+  charaTrade: {
+    ongoing: Boolean,
+    startedBy: String,
+    for: {
+      id: Number,
+      name: String,
+      source: String,
+    },
+    offer: {
+      id: Number,
+      name: String,
+      image: String,
+      about: String,
+      source: String,
+    },
+  },
 })
 
 export default model<IGroupModel>('groups', GroupSchema)
