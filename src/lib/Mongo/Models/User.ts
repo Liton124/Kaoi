50d49516a576b996f6daff24eac131d305a7edc1
Liton124 <92647972+Liton124@users.noreply.github.com>
2022-02-24 13:11:16 +0530

    Update User.ts

--- src/lib/Mongo/Models/User.ts
+++ src/lib/Mongo/Models/User.ts
@@ -2,25 +2,41 @@ import { Schema, model } from 'mongoose'
 import { IUserModel } from '../../../typings'
 
 const UserSchema = new Schema({
-    jid: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    ban: {
-        type: Boolean,
-        required: true,
-        default: false
-    },
-    warnings: {
-        type: Number,
-        required: true,
-        default: 0
-    },
-    Xp: {
-        type: Number,
-        required: true,
-        default: 0
-    }
+  jid: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  ban: {
+    type: Boolean,
+    required: true,
+    default: false,
+  },
+  warnings: {
+    type: Number,
+    required: true,
+    default: 0,
+  },
+  Xp: {
+    type: Number,
+    required: true,
+    default: 0,
+  },
+  wallet: {
+    type: Number,
+    required: true,
+    default: 0,
+  },
+  bank: {
+    type: Number,
+    required: true,
+    default: 0,
+  },
+  lastDaily: {
+    type: Number,
+  },
+  lastRob: {
+    type: Number,
+  },
 })
 export default model<IUserModel>('users', UserSchema)
