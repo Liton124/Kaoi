f80bf3d3f436d014015d3b2ca8810f1de4a585ed
Liton124 <92647972+Liton124@users.noreply.github.com>
2022-02-24 13:38:55 +0530

    Update DatabaseHandler.ts

--- src/Handlers/DatabaseHandler.ts
+++ src/Handlers/DatabaseHandler.ts
@@ -1,9 +1,9 @@
-import { IDBModels } from "../typings";
-import UserModel from "../lib/Mongo/Models/User";
-import GroupModel from "../lib/Mongo/Models/Group";
-import SessionModel from "../lib/Mongo/Models/Session";
-import DisabledCommandsModel from "../lib/Mongo/Models/DisabledCommands";
-import IFeatureModel from "../lib/Mongo/Models/Features";
+import { IDBModels } from "../typings"
+import UserModel from "../lib/Mongo/Models/User"
+import GroupModel from "../lib/Mongo/Models/Group"
+import SessionModel from "../lib/Mongo/Models/Session"
+import DisabledCommandsModel from "../lib/Mongo/Models/DisabledCommands"
+import IFeatureModel from "../lib/Mongo/Models/Features"
 import cdModel from "../lib/Mongo/Models/Countdown";
 
 export default class DatabaseHandler implements IDBModels {
