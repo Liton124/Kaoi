41b3e4c5602f07bbf2d286fbeeed1b3469277936
Liton124 <92647972+Liton124@users.noreply.github.com>
2022-02-26 15:23:10 +0530

    Update Profile.ts

--- src/commands/General/Profile.ts
+++ src/commands/General/Profile.ts
@@ -37,7 +37,7 @@ export default class Command extends BaseCommand {
         if (await (await this.client.getUser(user)).married) {
           haigusha = await (await this.client.getUser(user)).haigusha.name;
         } else {
-          haigusha = "None";
+          haigusha = "None (single)";
         }
         const xp = (await this.client.getUser(user)).Xp
         let role
@@ -107,7 +107,7 @@ export default class Command extends BaseCommand {
             undefined,
             `🏮 *Username: ${username}*\n\n🎫 *About: ${
                 (await this.client.getStatus(user)).status || 'None'
-            }*\n\n〽️ *Level: ${level}*\n\n🌟 *Xp: ${xp || 0}*\n\n🍁 *Role: ${role}*\n\n👑 *Admin: ${
+            }*\n\n❤ *Haigusha: ${haigusha}*\n\n〽️ *Level: ${level}*\n\n🌟 *Xp: ${xp || 0}*\n\n🍁 *Role: ${role}*\n\n👑 *Admin: ${
                 M.groupMetadata?.admins?.includes(user) || false
             }*\n\n❌ *Ban: ${(await this.client.getUser(user)).ban || false}*`
         )
