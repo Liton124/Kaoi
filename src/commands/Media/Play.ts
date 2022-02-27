c6f4b61114c2c4d1a3984f8df81ea6f17612c84f
Liton124 <92647972+Liton124@users.noreply.github.com>
2022-02-27 12:04:57 +0530

    Update Play.ts

--- src/commands/Media/Play.ts
+++ src/commands/Media/Play.ts
@@ -1,47 +1,56 @@
-import { MessageType } from '@adiwajshing/baileys'
-import MessageHandler from '../../Handlers/MessageHandler'
-import BaseCommand from '../../lib/BaseCommand'
-import WAClient from '../../lib/WAClient'
-import { IParsedArgs, ISimplifiedMessage } from '../../typings'
-import yts from 'yt-search'
-import YT from '../../lib/YT'
+import { MessageType } from "@adiwajshing/baileys";
+import MessageHandler from "../../Handlers/MessageHandler";
+import BaseCommand from "../../lib/BaseCommand";
+import WAClient from "../../lib/WAClient";
+import { IParsedArgs, ISimplifiedMessage } from "../../typings";
+import yts from "yt-search";
+import YT from "../../lib/YT";
 
 export default class Command extends BaseCommand {
-    constructor(client: WAClient, handler: MessageHandler) {
-        super(client, handler, {
-            command: 'play',
-            description: '🎵 play a song with just search term!',
-            category: 'media',
-            aliases: ['song'],
+  constructor(client: WAClient, handler: MessageHandler) {
+    super(client, handler, {
+      command: "play",
+      description: "🎵 play a song with just search term!",
+      category: "media",
+      aliases: ["music", "song"],
+      usage: `${client.config.prefix}play [term]`,
+      baseXp: 30,
+    });
+  }
 
-
-
-            usage: `${client.config.prefix}play [term]`,
-            baseXp: 30
-        })
-    }
-
-    run = async (M: ISimplifiedMessage, { joined }: IParsedArgs): Promise<void> => {
-        if (!joined) return void M.reply('🔎 Provide a search term')
-        const term = joined.trim()
-        const { videos } = await yts(term)
-        if (!videos || videos.length <= 0) return void M.reply(`⚓ No Matching videos found for the term : *${term}*`)
-        const audio = new YT(videos[0].url, 'audio')
-        if (!audio.url) return
-        M.reply('👾 Sending your song, wait for a while.')
-        this.client
-            .sendMessage(M.from, await audio.getBuffer(), MessageType.audio, {
-                quoted: M.WAMessage,
-                contextInfo: {
-                    externalAdReply: {
-                        title: videos[0].title.substr(0, 50),
-                        body: `author : ${videos[0].author.name.substr(0, 30)}\nSent Via : Kaoi`,
-                        mediaType: 2,
-                        thumbnailUrl: `https://i.ytimg.com/vi/${audio.id}/hqdefault.jpg`,
-                        mediaUrl: audio.url
-                    }
-                }
-            })
-            .catch((reason: Error) => M.reply(`❌ an error occurred, Reason: ${reason}`))
-    }
+  run = async (
+    M: ISimplifiedMessage,
+    { joined }: IParsedArgs
+  ): Promise<void> => {
+    if (!joined) return void M.reply(" Provide a search term, Baka!");
+    const term = joined.trim();
+    const { videos } = await yts(term);
+    if (!videos || videos.length <= 0)
+      return void M.reply(
+        `⚓ No Matching videos found for the term : *${term}*`
+      );
+    const audio = new YT(videos[0].url, "audio");
+    if (!audio.url) return;
+    this.client
+      .sendMessage(M.from, await audio.getBuffer(), MessageType.audio, {
+        quoted: M.WAMessage,
+        contextInfo: {
+          externalAdReply: {
+            title: videos[0].title.substr(0, 30),
+            body: `author : ${videos[0].author.name.substr(
+              0,
+              20
+            )}\n🌟 Kaoi 🌟`,
+            mediaType: 2,
+            thumbnail: await this.client.getBuffer(
+              `https://i.ytimg.com/vi/${audio.id}/hqdefault.jpg`
+            ),
+            mediaUrl: audio.url,
+          },
+        },
+      })
+      .catch((reason: Error) =>
+        M.reply(`✖ An error occurred. Please try again later.`)
+      );
+  };
 }
