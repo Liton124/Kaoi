92e99e9ccdf092ab43e75a9d2ff2977438c9c864
Liton124 <92647972+Liton124@users.noreply.github.com>
2022-02-27 18:04:48 +0530

    Update Wallet.ts

--- src/commands/Economy/Wallet.ts
+++ src/commands/Economy/Wallet.ts
@@ -28,8 +28,7 @@ export default class Command extends BaseCommand {
 
     const buttonMessage: any = {
       contentText: `👛 *Wallet | ${M.sender.username}*\n\n🪙 *Gold: ${result}*`,
-      footerText: "Kaoi🚀",
-      buttons: buttons,
+      footerText: "Kaoi 🚀",
       headerType: 1,
     };
     await M.reply(buttonMessage, MessageType.buttonsMessage);
